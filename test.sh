58177a568bbd74938b61a5152013cd8f777af756
John Blackbourn <john@humanmade.com>
2021-12-27 16:34:38 +0000

    Don't need this timing info.

--- test.sh
+++ test.sh
@@ -3,19 +3,17 @@
 # -o pipefail Produce a failure return code if any command errors
 set -o pipefail
 
-echo    "file                           | exception handler    | error handler        | shutdown handler     | time | exit code"
-echo -n "------------------------------ | -------------------- | -------------------- | -------------------- | ---- | ---------"
+echo    "file                           | exception handler    | error handler        | shutdown handler     | exit code"
+echo -n "------------------------------ | -------------------- | -------------------- | -------------------- | ---------"
 
 for f in error-*.php; do
-	start=$SECONDS
 	php \
 		-d display_errors=0 \
 		-d error_reporting=E_ALL \
 		-d memory_limit=4M \
 		-d max_execution_time=9 \
 		-f test.php $f \
-		&& echo -n "$(( SECONDS - start ))    | " \
-		|| echo -n "$(( SECONDS - start ))    | $?"
+		|| echo -n "$?"
 done
 
 echo ""
